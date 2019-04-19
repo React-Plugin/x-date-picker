ed2e7fc63cee250ef56c1fa96ec36d7448ec36f3
tianxiangbing <55342775@qq.com>
2019-04-19 14:19:46 +0800

    '0.0.1'

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # x-date-picker
-react date-picker component
+基于antd的组件
+
+### 关于作者
+[https://github.com/tianxiangbing](https://github.com/tianxiangbing)
---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,18 @@
+/*
+ * Created with Visual Studio Code.
+ * github: https://github.com/React-xui/x-date-picker
+ * User: 田想兵
+ * Date: 2017-05-14
+ * Time: 20:00:00
+ * Contact: 55342775@qq.com
+ */
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import DatePicker from 'antd/lib/date-picker';  // 加载 JS
+import 'antd/lib/date-picker/style/css';        // 加载 CSS
+// import Calendar from 'rc-calendar';
+// import 'rc-calendar/assets/index.css';
+export default DatePicker;
+// import DatePicker from 'rc-calendar/lib/Picker';
+// import RangePicker from 'rc-calendar/lib/range-calendar';
+// module.exports = { Calendar, DatePicker };
